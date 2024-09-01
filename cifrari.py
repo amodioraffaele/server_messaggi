9d38c08a35013ddcbdf74786d6a3e835a614b71b
amodioraffaele <134727817+amodioraffaele@users.noreply.github.com>
2024-09-01 19:01:13 +0200

    Update cifrari.py

--- cifrari.py
+++ cifrari.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa, utils, padding
 from dotenv import load_dotenv
 import os
 from Cryptodome.PublicKey import RSA
-
+from cryptography.hazmat.primitives import serialization
 
 
 def decifra(dati, chiave):
@@ -17,41 +17,16 @@ def decifra(dati, chiave):
 
 def cifraAES(dati, chiave):
     cifrario = AES.new(chiave, AES.MODE_ECB)
-    dat = cifrario.decrypt(pad(dati, 16))
-    return dat.decode(encoding="latin-1")
+    dat = cifrario.encrypt(pad(dati.encode(), 16))
+    return b64encode(dat)
+
+
 
 def decifraRSA(cifrato):
-    chiave = """-----BEGIN RSA PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCcZxohMIF3nx28
-PMlrYfCvRAAgQQmmImhpr4c9fzYyd9g6799FMnJKiMBwjnBwvXQdP0O7klXd1dJj
-HCM7dA9XyVOr94nLrqVkeFQ61xXOFwFnWzzUsf4D6eYfqXYK/LNcrxMe4Hubt0zP
-w5sPJIdBzbj890VOmQcYjLB5IeL2jDVHvVzOSNG2CwUxu9VXBx60f/dE8qZts7rn
-BLp5Ndw3UHaR4L3HS/jygOb2nr5seE9P/T0UU3KRkopv6ffxwycFXZLz91o8GnO5
-/armE07YR7ED2H/CZbakJbEWrXdNe9SWrodTpOmdX2rNjVvS9SlIixerd3518Jh9
-XZH7+purAgMBAAECggEAPoi8ulN2GHmOvpJBaIV+4dza/gpzDLGKNOOBO9XuugwP
-8TSyfSzX5LQ72WUiUPl3ATCn6MnwOiPBnG4GKkHT1qJBKatuNXmtWHgCGYqhBcdO
-AODKeDJ4oWD2aWdssqTqJB8+EmToF1EZTaLkjw/oYqeaFM7bL7tgynCKKMyjPjcd
-0VNtZnRm5Cxh7ct4GA9YP4mWXgfIOtpzNke0FGMdQdYdB/G3l8ZE4kZBjkHHeSOO
-bAG0cEWasR0SgJoTgtFHnxAgnpamiPcAC2Xq/KuaIhSeIxZJ5GdX+Esgk9IBb/35
-+GfAFQb41EH4GFfDzJCDjIFPbdG+XY8t2jX8UrZglQKBgQDUrnLj0/RVRBSG6V61
-wzT/h+5WehcylpdTdtvYhY0ABfXDradUddQJ1BrvXgdtofNL7q/EAv84o96LLn1Q
-H5evgzLZeFZER64YhlWRL5g15CxoN/m9a/30z80puHBDAUBAwBoXV4UXO/yAq/Dh
-xMQG6tkiErUdatzmUe76R81c3wKBgQC8QjEWteoyxC6PfsCq85V38pAdXpLNdM+9
-nUHk9wD7NkeL7y71HN6uKN/7gjmY/WjbOx8XhIDhBbHj/dZDx8lieJIUx063YNFO
-bI0AHYkYL1F1xOUz00E1CsRUKmt7FEXWBiuzniEuYlsFqML2jB7BoFmoHK/9n7yo
-oDDtDgROtQKBgQCEP7XRVYspOhxJh/QMskXSX4Qk4eZq3planR43lVQIH6yi8OiL
-7HjdY7ccASw9T+cp4FILYJGzdrJ7eX0SxZJc4QhNkjaSXsAzH9U1YpMTb77tT2FM
-GEriYBUDTF0CTVTW7p1KxnFL8VEh8cjnmqMKah56wYc9s6WI0on3t45LoQKBgGu+
-ZmHdE4CijxNJM/OjHTRc+uYULmiwJgUbb48fXsxnsGMCLRnTwA5lDmvfiB9rSQvP
-tme7Shd/LuSRboO0YCmfX9vMhdyl6KS9s6URQlk5G6IPYi5bBcLZuquA66qxW5a2
-SWSvZ8YoPHfsskhwwVmH8Egqwv6g4VSQ+e9ySjzRAoGAdzy6jq6NxVgS1mHavChs
-GbnIgWo2J7Gs7plxBpCubmqwxF5HcRRRAaEKIUJ1BY092u5hjc9glJisomWmjdGe
-j7HcbWfbQ02npeKjKjb31dIA2N2/4mPaXiPPcAqL9CPS/iOnISQ0e9avs+koTptI
-pe+kRD4J/76buZsa5m4vCG4=
------END RSA PRIVATE KEY-----"""
-    private_key = RSA.import_key(chiave)
-    cipher = PKCS1_v1_5.new(private_key)
-    PlainText = cipher.decrypt(cifrato, "errore")
+    with open("server\chiave-priv.pem", "rb") as f:
+        chiave = serialization.load_pem_private_key(f.read(), None)
+    cifrario = PKCS1_v1_5.new(chiave)
+    PlainText = cifrario.decrypt(cifrato, "errore")
     return PlainText
 
 def cifraRSA(dati):
