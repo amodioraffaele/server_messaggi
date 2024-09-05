f1a3288d2353d15513ce2f8df39ec10176c3173b
amodioraffaele <134727817+amodioraffaele@users.noreply.github.com>
2024-09-05 15:26:03 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,40 +1,32 @@
 from fastapi import FastAPI
 import database
 import cifrari
-import re
-from base64 import b64decode, b64encode
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization, hashes
-import datetime
-import textwrap
+from base64 import b64decode
 from pydantic import BaseModel
 
 
 
-Chiavi = database.Chiavi()
+Database = database.Database()
 app = FastAPI()
 
 class MESSAGGIO_ARRIVO(BaseModel):
     cifratoAES: str
     ChiaveCifrata: str
     
-class AUTENTICAZIONE(BaseModel):
-    cifratoAES: str
-    ChiaveCifrata: str
-    API_KEY: str
 
 
 @app.post("/registrazione")
 async def registrazione(mess : MESSAGGIO_ARRIVO):
-    try:
-        chiaveAES = cifrari.decifraRSA(b64decode(mess.ChiaveCifrata))
-        dati = cifrari.decifra(mess.cifratoAES, chiaveAES)
-        Prefisso,Numero,password = dati.split(" ")
-        messaggio = Chiavi.registra(Prefisso,Numero, password)
-        messaggio = cifrari.cifraAES(messaggio, chiaveAES)
-        return {"risposta": messaggio}
-    except Exception as e:
-        return {"risposta": "Errore"}
+        try:
+            chiaveAES = cifrari.decifraRSA(b64decode(mess.ChiaveCifrata))
+            dati = cifrari.decifra(mess.cifratoAES, chiaveAES)
+            Prefisso,Numero,password = dati.split(" ")
+            messaggio = Database.registra(Prefisso,Numero, password)
+            messaggio = cifrari.cifraAES(messaggio, chiaveAES)
+            return {"risposta": messaggio}
+        except Exception as e:
+            return {"risposta": "Errore"}
+
 
 
 
@@ -43,10 +35,8 @@ async def login(login : MESSAGGIO_ARRIVO):
     try:
         chiaveAES = cifrari.decifraRSA(b64decode(login.ChiaveCifrata))
         dati = cifrari.decifra(login.cifratoAES, chiaveAES)
-        dati = re.sub("\s\s+", " ", dati)
         Prefisso,Numero,password = dati.split(" ")
-        messaggio = Chiavi.login(Prefisso,Numero, password)
-        print(messaggio)
+        messaggio = Database.login(Prefisso,Numero, password)
         messaggio = cifrari.cifraAES(messaggio, chiaveAES)
         return {"risposta": messaggio}
     except Exception as e:
@@ -60,7 +50,8 @@ async def cerca(mess: MESSAGGIO_ARRIVO):
     try:
         chiaveAES = cifrari.decifraRSA(b64decode(mess.ChiaveCifrata))
         numero = cifrari.decifra(mess.cifratoAES, chiaveAES)
-        messaggio = Chiavi.Cerca(numero)
+        messaggio = Database.Cerca(numero)
+        print(messaggio)
         messaggio = cifrari.cifraAES(messaggio, chiaveAES)
         return {"risposta" : messaggio}
     except Exception as e:
@@ -75,7 +66,7 @@ async def reg_id(mess: MESSAGGIO_ARRIVO):
         chiaveAES = cifrari.decifraRSA(b64decode(mess.ChiaveCifrata))
         dati = cifrari.decifra(mess.cifratoAES, chiaveAES)
         Prefisso,Numero,firebaseid = dati.split(" ")
-        messaggio = Chiavi.registra_id(Prefisso,Numero, firebaseid)
+        messaggio = Database.registra_id(Prefisso,Numero, firebaseid)
         messaggio = cifrari.cifraAES(messaggio, chiaveAES)
         return {"risposta" : messaggio} 
     except Exception as e:
@@ -84,12 +75,12 @@ async def reg_id(mess: MESSAGGIO_ARRIVO):
 
 
 @app.post("/chiave")
-async def chiave(mess: AUTENTICAZIONE):
+async def chiave(mess: MESSAGGIO_ARRIVO):
     try:
         chiaveAES = cifrari.decifraRSA(b64decode(mess.ChiaveCifrata))
         dati = cifrari.decifra(mess.cifratoAES, chiaveAES)
         id1,id2, API_KEY = dati.split(" ")
-        messaggio = Chiavi.salvachiave(id1,id2, API_KEY)
+        messaggio = Database.salvachiave(id1,id2, API_KEY)
         messaggio = cifrari.cifraAES(messaggio, chiaveAES)
         return {"risposta": messaggio}
     except Exception as e:
@@ -103,10 +94,15 @@ async def cerca_id(mess: MESSAGGIO_ARRIVO):
     try:
         chiaveAES = cifrari.decifraRSA(b64decode(mess.ChiaveCifrata))
         id = cifrari.decifra(mess.cifratoAES, chiaveAES)
-        messaggio = Chiavi.Cerca_id(id)
+        messaggio = Database.Cerca_id(id)
         messaggio = cifrari.cifraAES(messaggio, chiaveAES)
         return {"risposta": messaggio}
     except Exception as e:
         return {"risposta": "Errore"}
+
+
+
+
+
 
 
